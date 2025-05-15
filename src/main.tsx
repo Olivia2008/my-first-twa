7461d52de23d4f4168a33661fa26055e402b6a60
olivia <xuelianhua-bj2008@hotmail.com>
2025-05-15 18:10:14 +0800

    fix manifest

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://olivia2008.github.io/my-first-twa';
 createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
